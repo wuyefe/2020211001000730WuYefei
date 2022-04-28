eb9e46110ef090ce11efab0e8a18b8323df0b9c3
wuyefei <1511315277@qq.com>
2022-04-28 16:09:52 +0800

    week 9

---
+++ src/main/java/com/wuyefei/week6/RedirectServlet.java
@@ -0,0 +1,19 @@
+package com.wuyefei.week6;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RedirectServlet", value = "/RedirectServlet")
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.sendRedirect("https://github.com/");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
